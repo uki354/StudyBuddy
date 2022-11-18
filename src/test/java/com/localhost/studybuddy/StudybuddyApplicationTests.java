2ef8e75e857ea9cc2227ec3fdc53f5d2fab86550
uki <uros.3.stevanovic@gmail.com>
2022-11-18 19:18:06 +0100

    Initial commit

---
+++ src/test/java/com/localhost/studybuddy/StudybuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localhost.studybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudybuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
