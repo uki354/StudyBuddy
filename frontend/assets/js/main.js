3b7f9c8bce71a6a3e071570a6b25a3b03eab11eb
Luka <lukamuticxy@gmail.com>
2022-11-20 00:23:51 +0100

    working with login and registration system

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
-    $(".users-listing-section .request-btn").on("click", function (event) {
-
+    $(".request-btn").on("click", function () {
         $(this).addClass("send");
         $(this).find(".request").addClass("d-none");
         $(this).find(".send").removeClass("d-none").addClass("d-block");
@@ -23,6 +22,123 @@ $(document).ready(function () {
         $(".about-being .yes-about").addClass("d-none");
         return false;
     });
+
+
+    // https://app-studdy-buddy.herokuapp.com/api/
+
+
+    // let xhttp = new XMLHttpRequest();
+    //
+    // xhttp.onreadystatechange = function (){
+    //     if( this.readyState == 4 && this.status == 200){
+    //
+    //         console.log(this.responseText);
+    //
+    //     }
+    // }
+    //
+    // xhttp.open("GET", "https://app-studdy-buddy.herokuapp.com/api/", true );
+    // xhttp.send();
+
+
+//     var jqxhr = $.ajax( "https://app-studdy-buddy.herokuapp.com/api/" )
+//         .done(function() {
+//             alert( "success" );
+//         })
+//         .fail(function() {
+//             alert( "error" );
+//         })
+//         .always(function() {
+//             alert( "complete" );
+//         });
+//
+// // Perform other work here ...
+//
+// // Set another completion function for the request above
+//     jqxhr.always(function() {
+//         alert( "second complete" );
+//     });
+
+
+    // $.postJSON = function(url,data, callback) {
+    //     return jQuery.ajax({
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json'
+    //         },
+    //         'type': 'POST',
+    //         'url': url,
+    //         'data': JSON.stringify(data),
+    //         'dataType': 'json',
+    //         'success': callback
+    //     });
+    // };
+
+
+    // Registration
+    $("#register-btn").on("click", function () {
+        var email = $("#email").val();
+        var name = $("#name").val();
+        var lastname = $("#lastname").val();
+        var university = $("#faculty").val();
+        var birth = $("#start").val();
+        var password = $("#password").val();
+
+
+        var register = {
+            "email": email,
+            "name": name,
+            "lastname": lastname,
+            "university": university,
+            "password": password,
+            "birthdate": birth,
+        }
+
+        console.log(register);
+
+        $.ajax({
+            url: 'https://app-studdy-buddy.herokuapp.com/api/user/create',
+            type: 'post',
+            contentType: 'application/json',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(register),
+            success: function (data) {
+                console.log(data);
+            }
+        });
+    })
+
+
+    // Login
+
+    $("#login-btn").on("click", function () {
+
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        var login = {
+            "email": email,
+            "password": password
+        }
+
+        $.ajax({
+            url: 'https://app-studdy-buddy.herokuapp.com/api/user/login',
+            type: 'post',
+            contentType: 'application/json',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(login),
+            success: function (data) {
+                console.log(data);
+            }
+        });
+
+    })
 
 
 });
