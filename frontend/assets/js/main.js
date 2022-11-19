2ec27a8786345145cce8bb979f16b4c3a07695ca
Luka <lukamuticxy@gmail.com>
2022-11-19 20:49:09 +0100

    added pages: be-study-buddy.html, be_or_not.html, go-to-study-buddy.html

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
 
-    $(".users-listing-section .request-btn").on("click", function () {
+    $(".users-listing-section .request-btn").on("click", function (event) {
+
         $(this).addClass("send");
         $(this).find(".request").addClass("d-none");
         $(this).find(".send").removeClass("d-none").addClass("d-block");
+        return false;
+    });
+
+
+    // be studdyBuddy or find studybuddy:
+
+    $(".yes-btn").on("click", function () {
+        $(".about-being .yes-about").removeClass("d-none");
+        $(".about-being").removeClass("invisible");
+        $(".about-being .no-about").addClass("d-none");
+        return false;
+    });
+    $(".no-btn").on("click", function () {
+        $(".about-being").removeClass("invisible");
+        $(".about-being .no-about").removeClass("d-none");
+        $(".about-being .yes-about").addClass("d-none");
+        return false;
     });
 
 
